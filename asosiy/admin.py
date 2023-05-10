399b2203d11b430375051a8f1693ea0825737d5e
Akmaljon Fayzullayev <akmaljonfayzullayev07@gmail.com>
2023-05-10 23:10:35 +0500

    Models qoshildi

--- asosiy/admin.py
+++ asosiy/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Oqituvchi)
+admin.site.register(Oquvchi)
+admin.site.register(Sinf)
+admin.site.register(Yangiliklar)
+admin.site.register(Reyting_daftar)
+admin.site.register(Murojat)
+admin.site.register(Malumot)
+admin.site.register(MaktabMedia)
--- asosiy/models.py
+++ asosiy/models.py
@@ -1,3 +1,65 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Oqituvchi(models.Model):
+    fish = models.CharField(max_length=60)
+    fan = models.CharField(max_length=50)
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.fish
+
+class Sinf(models.Model):
+    bosqich = models.PositiveSmallIntegerField()
+    rang = models.CharField(max_length=20)
+    kurator = models.ForeignKey(Oqituvchi, on_delete=models.CASCADE)
+    def __str__(self):
+        return f"{self.bosqich}-{self.rang}"
+
+class Oquvchi(models.Model):
+    fish = models.CharField(max_length=60)
+    sinf = models.ForeignKey(Sinf, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.fish
+
+class Reyting_daftar(models.Model):
+    oquvchi = models.ForeignKey(Oquvchi, on_delete=models.CASCADE)
+    chorak_1 = models.PositiveSmallIntegerField()
+    chorak_2 = models.PositiveSmallIntegerField()
+    chorak_3 = models.PositiveSmallIntegerField()
+    chorak_4 = models.PositiveSmallIntegerField()
+    yillik  =models.PositiveSmallIntegerField()
+    imtihon = models.PositiveSmallIntegerField()
+    natija = models.CharField(max_length=50)
+    oquv_yili = models.CharField(max_length=20)
+    def __str__(self):
+        return f"{self.oquvchi}({self.oquv_yili})"
+
+
+class Yangiliklar(models.Model):
+    sarlavha = models.CharField(max_length=60)
+    matn = models.TextField()
+    rasm = models.FileField()
+    sana = models.DateTimeField(auto_now_add=True)
+    def __str__(self):
+        return self.sarlavha
+
+class Malumot(models.Model):
+    sarlavha = models.CharField(max_length=60)
+    bepul = models.BooleanField(default=False)
+    rasm = models.FileField()
+
+class MaktabMedia(models.Model):
+    rasm = models.FileField()
+    sarlavha = models.CharField(max_length=60)
+    def __str__(self):
+        return self.sarlavha
+
+class Murojat(models.Model):
+    ism = models.CharField(max_length=50)
+    email = models.EmailField()
+    tel = models.CharField(max_length=15)
+    sarlavha = models.CharField(max_length=60)
+    xabar = models.TextField()
+    def __str__(self):
+        return f"{self.ism}({self.email})"
