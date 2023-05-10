1c9dc7e46a5aed920c8355531ac43101ce542d3e
Akmaljon <akmalkhanever@gmail.com>
2023-05-10 23:43:16 +0500

    pull qivororing

--- Maktab/urls.py
+++ Maktab/urls.py
@@ -7,13 +7,4 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
-    path('bosh_sahifa/', bosh_sahifa),
-    path('yangiliklar/', yangiliklar),
-    path('media/', maktab_hayoti),
-    path('jurnal/', journal),
-    path('jurnalitem/', journalitems),
-    path('statistika/', statistika),
-    path('puplis/', puplis),
-    path('contact/', contact),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
